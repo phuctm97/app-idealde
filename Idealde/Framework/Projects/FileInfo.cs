0e00d0593064dd700d5552d175efb316b1adaa0b
phuctm97 <phuctm97@gmail.com>
2017-06-17 13:56:04 +0700

    fix

--- Idealde/Framework/Projects/FileInfo.cs
+++ Idealde/Framework/Projects/FileInfo.cs
@@ -1,31 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Idealde.Framework.Projects
+﻿namespace Idealde.Framework.Projects
 {
     public class FileInfo
     {
-        public string Name
-        {
-            set;
-            get;
-        }
-        public string VirtualAddress
-        {
-            set;
-            get;
-        }
+        public string Name { set; get; }
 
-        public string MemoryAddress
-        {
-            set;
-            get;
-        }
+        public string VirtualAddress { set; get; }
 
-        public FileInfo ( string name, string virtualAddress, string memoryAddress )
+        public string MemoryAddress { set; get; }
+
+        public FileInfo(string name, string virtualAddress, string memoryAddress)
         {
             Name = name;
             VirtualAddress = virtualAddress;
--- Idealde/Framework/Projects/ProjectController.cs
+++ Idealde/Framework/Projects/ProjectController.cs
@@ -3,9 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Xml.Linq;
-
 using Caliburn.Micro;
-
 using Idealde.Framework.Projects;
 
 #endregion
@@ -15,18 +13,18 @@ namespace Idealde.Framework.ProjectExplorer.Models
     public class ProjectController : IProjectController
     {
         private readonly Dictionary<Type, ProjectItemDefinition> _projectItemTypeToDefinitionLookup;
-        public List < FileInfo > Files;
-        public List < string > Folders;
-        public List < string > LibraryFiles;
-        public List < string > OutputType;
+        public List<FileInfo> Files { get; }
+        public List<string> Folders { get; }
+        public List<string> LibraryFiles { get; }
+        public List<string> OutputType { get; }
 
         public ProjectController()
         {
             _projectItemTypeToDefinitionLookup = new Dictionary<Type, ProjectItemDefinition>();
-            Files=new List < FileInfo > ();
-            Folders=new List < string > ();
-            LibraryFiles=new List < string > ();
-            OutputType =new List < string > ();
+            Files = new List<FileInfo>();
+            Folders = new List<string>();
+            LibraryFiles = new List<string>();
+            OutputType = new List<string>();
         }
 
         public ProjectItemDefinition GetProjectItemDefinition(Type projectItemDefinitionType)
@@ -40,37 +38,36 @@ namespace Idealde.Framework.ProjectExplorer.Models
             return projectItemDefinition;
         }
 
-        public void Load (string path)
+        public void Load(string path)
         {
-            XElement projectFile=XElement.Load(path);
+            var projectFile = XElement.Load(path);
 
-            foreach ( var file in projectFile.Descendants("FileItem") )
+            foreach (var file in projectFile.Descendants("FileItem"))
             {
-                Files.Add(new FileInfo(file.Element("Name").Value.ToString(),
-                    file.Element("VitualAdsress").Value.ToString(),
-                    file.Element("MemoryAddress").Value.ToString()));
+                Files.Add(new FileInfo(file.Element("Name")?.Value,
+                    file.Element("VitualAddress")?.Value,
+                    file.Element("MemoryAddress")?.Value));
             }
 
-            foreach ( var folder in projectFile.Descendants("FolderGroup") )
+            foreach (var folder in projectFile.Descendants("FolderGroup"))
             {
-                Folders.Add(folder.Value.ToString());
+                Folders.Add(folder.Value);
             }
 
             foreach (var libFile in projectFile.Descendants("LibraryFileGroup"))
             {
-                LibraryFiles.Add(libFile.Value.ToString());
+                LibraryFiles.Add(libFile.Value);
             }
 
-            foreach ( var outputType in projectFile.Descendants("OutputGroup") )
+            foreach (var outputType in projectFile.Descendants("OutputGroup"))
             {
-                OutputType.Add(outputType.Value.ToString());
+                OutputType.Add(outputType.Value);
             }
-
         }
 
-        public void Save ( string path )
+        public void Save(string path)
         {
-            XDocument projectFile= new XDocument(
+            var projectFile = new XDocument(
                 new XDeclaration("1.0", "UTF-8", null),
                 new XElement("Project",
                     new XElement("FileGroup"),
@@ -79,29 +76,28 @@ namespace Idealde.Framework.ProjectExplorer.Models
                     new XElement("OutputGroup")
                 ));
 
-            foreach ( var fileInfo in Files )
+            foreach (var fileInfo in Files)
             {
-
-                projectFile.Root.Element("FileGroup").Add(new XElement("FileItem",
+                projectFile.Root?.Element("FileGroup")?.Add(new XElement("FileItem",
                     new XElement("Name", fileInfo.Name),
-                    new XElement("VitualAdsress", fileInfo.VirtualAddress),
+                    new XElement("VitualAddress", fileInfo.VirtualAddress),
                     new XElement("MemoryAddress", fileInfo.MemoryAddress)
                 ));
             }
 
-            foreach ( var folder in Folders )
+            foreach (var folder in Folders)
             {
-                projectFile.Root.Element("FolderGroup").Add(new XElement("FolderItem", folder));
+                projectFile.Root?.Element("FolderGroup")?.Add(new XElement("FolderItem", folder));
             }
 
-            foreach ( var libraryFile in LibraryFiles )
+            foreach (var libraryFile in LibraryFiles)
             {
-                projectFile.Root.Element("LibraryFileGroup").Add(new XElement("FolderItem", libraryFile));
+                projectFile.Root?.Element("LibraryFileGroup")?.Add(new XElement("FolderItem", libraryFile));
             }
 
             foreach (var outputType in OutputType)
             {
-                projectFile.Root.Element("OutputGroup").Add(new XElement("FolderItem", outputType));
+                projectFile.Root?.Element("OutputGroup")?.Add(new XElement("FolderItem", outputType));
             }
 
             projectFile.Save(path);
