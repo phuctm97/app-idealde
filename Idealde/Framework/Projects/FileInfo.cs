71031fe2ebf3b6885cc1b2940e3df29b35b51284
Nhattd97 <nhattd97@gmail.com>
2017-06-17 13:19:57 +0700

    project file

---
+++ Idealde/Framework/Projects/FileInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idealde.Framework.Projects
+{
+    public class FileInfo
+    {
+        public string Name
+        {
+            set;
+            get;
+        }
+        public string VirtualAddress
+        {
+            set;
+            get;
+        }
+
+        public string MemoryAddress
+        {
+            set;
+            get;
+        }
+
+        public FileInfo ( string name, string virtualAddress, string memoryAddress )
+        {
+            Name = name;
+            VirtualAddress = virtualAddress;
+            MemoryAddress = memoryAddress;
+        }
+    }
+}
