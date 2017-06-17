f39b364cef5b93ed5ae88d9ab2ca7f22227599fc
phuctm97 <phuctm97@gmail.com>
2017-06-17 21:19:28 +0700

    fix for menu context command

---
+++ Idealde/Modules/ProjectExplorer/Commands/FakeCommandDefinition.cs
@@ -0,0 +1,14 @@
+﻿using Idealde.Framework.Commands;
+
+namespace Idealde.Modules.ProjectExplorer.Commands
+{
+    public class FakeCommandDefinition : CommandDefinition
+    {
+        public FakeCommandDefinition(string name)
+        {
+            Name = name;
+        }
+
+        public override string Name { get; }
+    }
+}
--- Idealde/Modules/ProjectExplorer/ViewModels/ProjectExplorerViewModel.cs
+++ Idealde/Modules/ProjectExplorer/ViewModels/ProjectExplorerViewModel.cs
@@ -1,10 +1,10 @@
 ﻿#region Using Namespace
 
-using System.IO;
-using System.Windows.Input;
+using System.Linq;
 using Caliburn.Micro;
 using Idealde.Framework.Panes;
 using Idealde.Modules.MainMenu.Models;
+using Idealde.Modules.ProjectExplorer.Commands;
 using Idealde.Modules.ProjectExplorer.Models;
 
 #endregion
@@ -21,9 +21,9 @@ namespace Idealde.Modules.ProjectExplorer.ViewModels
             DisplayName = "Solution Explorer";
             Items = new BindableCollection<ProjectItemBase>();
 
-            MenuItems = new BindableCollection<MenuItemBase>()
+            MenuItems = new BindableCollection<MenuItemBase>
             {
-                new DisplayMenuItem("OpenMenu","Open")
+                new DisplayMenuItem("OpenMenu", "Open")
             };
         }
 
@@ -33,13 +33,60 @@ namespace Idealde.Modules.ProjectExplorer.ViewModels
         public void PopulateMenu(ProjectItemBase item)
         {
             MenuItems.Clear();
+
+            MenuItemBase parentMenuItem = null;
+
             foreach (var command in item.OptionCommands)
             {
-                MenuItems.Add(new CommandMenuItem(string.Empty, command));
+                MenuItemBase newMenuItem = null;
+
+                // fake command definition
+                if (command.CommandDefinition is FakeCommandDefinition)
+                {
+                    // fake to add separator
+                    if (command.CommandDefinition.Name == "|")
+                    {
+                        newMenuItem = new MenuItemSeparator(string.Empty);
+                    }
+                    // fake to set parent
+                    else if (command.CommandDefinition.Name.Contains('|'))
+                    {
+                        // extract ancestors
+                        var parentNames = command.CommandDefinition.Name.Trim().Split(new[] {'|'},
+                            System.StringSplitOptions.RemoveEmptyEntries);
+
+                        // find parent
+                        if (parentNames.Length > 0)
+                        {
+                            var parentToFind = MenuItems.FirstOrDefault(p => p.Name == parentNames.First());
+                            for (var i = 1; i < parentNames.Length; i++)
+                            {
+                                if (parentToFind == null) break;
+                                parentToFind = parentToFind.Children.FirstOrDefault(p => p.Name == parentNames[i]);
+                            }
+                            if (parentToFind != null) parentMenuItem = parentToFind;
+                        }
+                    }
+                    // fake to add display item
+                    else
+                    {
+                        newMenuItem = new DisplayMenuItem(command.CommandDefinition.Name, command.Text);
+                    }
+                }
+
+                if (newMenuItem == null) continue;
+
+                if (parentMenuItem != null)
+                {
+                    parentMenuItem.Children.Add(newMenuItem);
+                }
+                else
+                {
+                    MenuItems.Add(newMenuItem);
+                }
             }
         }
 
         public IObservableCollection<MenuItemBase> MenuItems { get; }
-
     }
 }
